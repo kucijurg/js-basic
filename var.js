64cc8f92a68aad0b1e17ae3708a503a7516a69e5
jurgita <kuci.jurg@gmail.com>
2025-02-04 14:16:48 +0200

    kintamieji

---
+++ var.js
@@ -0,0 +1,29 @@
+const n1=1;
+console.log(n1);
+
+const n2=-1;
+console.log(n2);
+
+
+const n3=3.14;
+console.log(n3);
+
+const n4=-3.14;
+console.log(n4);
+//yra teigiamos ir neigiamos begalybes
+const n5=Infinity;
+console.log(n5);
+const n6=-Infinity;
+console.log(n6);
+// skaiciaus tipo
+const n8=NaN;
+
+console.log('labas isves nan'/ n1);
+//+ sujungia
+console.log('labas'+ n1);
+//+ sujungia
+//** */
+const n9=2;
+console.log(n9**0);
+//+ sujungia
+//matematine biblioteka Math
